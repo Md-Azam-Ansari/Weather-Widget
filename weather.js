8e562e9092c5e5ca5bf76e323e4285697a0afe04
MD Azam Ansari <azam.ansari.ind@outlook.com>
2023-10-18 22:28:36 +0530

    added index.html

---
+++ weather.js
@@ -0,0 +1,12 @@
+let cityValue = document.getElementById('cityValue')
+let cityName = document.getElementById('cityName')
+let countryName = document.getElementById('countryName')
+let temperature = document.getElementById('temperature')
+
+let search = () => {
+    if (cityValue.value.length == 0) {
+        alert('Enter City Name to Search')
+    } else {
+
+    }
+}
