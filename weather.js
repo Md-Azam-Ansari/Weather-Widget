84f18b7f81ce333481ab78f723832a61e02b6a67
MD Azam Ansari <azam.ansari.ind@outlook.com>
2023-10-19 19:41:32 +0530

    updated the whole project

--- weather.js
+++ weather.js
@@ -1,12 +1,34 @@
-let cityValue = document.getElementById('cityValue')
-let cityName = document.getElementById('cityName')
-let countryName = document.getElementById('countryName')
+let logo = document.getElementById('logo')
+
+
+let cityvalue = document.getElementById('cityvalue')
+let cityname = document.getElementById('cityname')
+let countryname = document.getElementById('countryname')
 let temperature = document.getElementById('temperature')
 
-let search = () => {
-    if (cityValue.value.length == 0) {
-        alert('Enter City Name to Search')
+let search = async () => {
+    if (cityvalue.value.length == 0) {
+        alert('please enter a city name')
     } else {
-
+        //api call
+        try {
+            let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityvalue.value + '&units=metric&appid=1cb6532aea3c298a830a71380eace21e'
+            let response = await fetch(url)
+            let data = await response.json()
+            cityname.textContent = data.name
+            countryname.textContent = data.sys.country
+            temperature.textContent = data.main.temp
+            if (temperature < 30) {
+                logo.src = 'https://media.giphy.com/media/zotwjIGhEIUzC/giphy.gif'
+            } else if (temperature > 20) {
+                logo.src = 'https://media.giphy.com/media/129BtnUUD6Lrzi/giphy.gif'
+            } else {
+                logo.src = 'https://media.giphy.com/media/s4Bi420mMDRBK/giphy.gif'
+            }
+        } catch {
+            alert('please enter a valid city name')
+        }
     }
 }
+
+
